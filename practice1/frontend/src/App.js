2800aa2fa3f2e4ed01687267c942a704d8bba02d
manjumary321 <106730320+manjumary321@users.noreply.github.com>
2022-11-30 17:39:32 +0530

    googlelogin2

---
+++ practice1/frontend/src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from "react";
+import './App.css';
+// import Login from './googlelogin.js';
+import Login2 from './googlelogin2.js';
+// import Loginhook from './googleloginhook.js';
+
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login2/>}></Route>
+          {/* <Route path="/Loginhook" element={<Loginhook/>}></Route>
+          <Route path="/login2" element={<Login2/>}></Route> */}
+        </Routes>
+      </BrowserRouter>
+
+    </div>
+  );
+}
+
+export default App;
