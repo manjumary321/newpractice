cc0ec652171f146d354a1fff9cf73ed78ab949f1
manjumary321 <106730320+manjumary321@users.noreply.github.com>
2022-12-01 10:49:58 +0530

    fblogin

--- practice1/frontend/src/App.js
+++ practice1/frontend/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import './App.css';
 // import Login from './googlelogin.js';
 import Login2 from './googlelogin2.js';
+import Fblogin from './fblogin.js';
 // import Loginhook from './googleloginhook.js';
 
 
@@ -12,6 +13,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login2/>}></Route>
+          <Route path="/Fblogin" element={<Fblogin/>}></Route>
           {/* <Route path="/Loginhook" element={<Loginhook/>}></Route>
           <Route path="/login2" element={<Login2/>}></Route> */}
         </Routes>
